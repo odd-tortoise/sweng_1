5c591ecd25b5ac3db09d7a337947229c474e4a8d
Giuseppe Starnini <126173923+Sterning171177@users.noreply.github.com>
2024-06-05 11:54:35 +0200

    Update test_matrix_multiplication.cpp

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -6,6 +6,22 @@
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
 
+
+
+/*This is the classical example of multiplication. There are no constraints.
+we were able to catch the following errors:
+
+Error 6: Result matrix contains a number bigger than 100!
+Error 6: Result matrix contains a number bigger than 100!
+Error 6: Result matrix contains a number bigger than 100!
+Error 6: Result matrix contains a number bigger than 100!
+Error 12: The number of rows in A is equal to the number of columns in B!
+Error 14: The result matrix C has an even number of rows!
+Error 19: Every row in matrix A contains the number 8!
+Error 20: Number of columns in matrix A is odd!
+
+we understand erros come from structure of matrix or from its elements.*/
+
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {8, 2, 3},
@@ -27,6 +43,29 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
+
+
+
+
+
+
+/*The test's aim is to verify the correctness of the simplest type of multiplication. It wants to test the neutral element.
+we were able to catch the following errors:
+
+Error 9: Result matrix contains the number 99!
+Error 7: Result matrix contains a number between 11 and 20!
+Error 7: Result matrix contains a number between 11 and 20!
+Error 9: Result matrix contains the number 99!
+Error 7: Result matrix contains a number between 11 and 20!
+Error 7: Result matrix contains a number between 11 and 20!
+Error 7: Result matrix contains a number between 11 and 20!
+Error 11: Every row in matrix B contains at least one '0'!
+Error 12: The number of rows in A is equal to the number of columns in B!
+Error 17: Result matrix C contains the number 17!
+Error 18: Matrix A is a square matrix!
+Error 20: Number of columns in matrix A is odd!
+
+the simplest multiplication fails. It is difficult that more sofisticated types of multiplication are correct. Previous errors are about matrix structure and matrix elements.*/
 
 
 TEST(MatrixMultiplicationTest, TestMultiplyIdentity) {
@@ -52,6 +91,34 @@ TEST(MatrixMultiplicationTest, TestMultiplyIdentity) {
 
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
+
+
+
+
+
+
+
+/*The test aims to verify the existence of the null multiplication element.
+we were able to catch the following errors:
+
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 8: Result matrix contains zero!
+Error 10: A row in matrix A contains more than one '1'!
+Error 11: Every row in matrix B contains at least one '0'!
+Error 12: The number of rows in A is equal to the number of columns in B!
+Error 15: A row in matrix A is filled entirely with 5s!
+Error 18: Matrix A is a square matrix!
+Error 20: Number of columns in matrix A is odd!
+
+results show the that zero matrix is not the null multiplication element.*/
+
 
 
 TEST(MatrixMultiplicationTest, TestMultipyZeros) {
@@ -80,6 +147,35 @@ TEST(MatrixMultiplicationTest, TestMultipyZeros) {
 
 
 
+
+
+
+
+
+
+/*The test aims to discover eventually changing when matrix is composed of negative numbers:
+The result is still wrong and we were able to catch the following errors:
+
+Error 3: Matrix A contains a negative number!
+Error 5: Matrix B contains a negative number!
+Error 5: Matrix B contains a negative number!
+Error 3: Matrix A contains a negative number!
+Error 5: Matrix B contains a negative number!
+Error 6: Result matrix contains a number bigger than 100!
+Error 3: Matrix A contains a negative number!
+Error 5: Matrix B contains a negative number!
+Error 3: Matrix A contains a negative number!
+Error 5: Matrix B contains a negative number!
+Error 6: Result matrix contains a number bigger than 100!
+Error 5: Matrix B contains a negative number!
+Error 3: Matrix A contains a negative number!
+Error 3: Matrix A contains a negative number!
+Error 12: The number of rows in A is equal to the number of columns in B!
+Error 14: The result matrix C has an even number of rows!
+Error 20: Number of columns in matrix A is odd!*/
+
+
+
 TEST(MatrixMultiplicationTest, TestNegativeNumbers) {
     std::vector<std::vector<int>> A = {
         {-1, 2, 3},
@@ -103,6 +199,35 @@ TEST(MatrixMultiplicationTest, TestNegativeNumbers) {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+/*The last test try to find a relation between a matrix and its transpose:
+The result is still wrong and we were able to catch the following errors:
+
+
+Error 1: Element-wise multiplication of ones detected!
+Error 4: Matrix B contains the number 3!
+Error 4: Matrix B contains the number 3!
+Error 6: Result matrix contains a number bigger than 100!
+Error 2: Matrix A contains the number 7!
+Error 4: Matrix B contains the number 3!
+Error 2: Matrix A contains the number 7!
+Error 6: Result matrix contains a number bigger than 100!
+Error 2: Matrix A contains the number 7!
+Error 6: Result matrix contains a number bigger than 100!
+Error 12: The number of rows in A is equal to the number of columns in B!
+Error 13: The first element of matrix A is equal to the first element of matrix B!
+Error 16: Matrix B contains the number 6!
+Error 18: Matrix A is a square matrix!
+Error 20: Number of columns in matrix A is odd!*/
 
 TEST(MatrixMultiplicationTest, TestMultiplyTranspose) {
     std::vector<std::vector<int>> A = {
